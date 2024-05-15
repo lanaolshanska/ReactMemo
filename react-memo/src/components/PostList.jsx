6ff2707e5ec05faff95443f96f0fb50b9e0124a8
Svitlana Olshanska <svitlana.olshanska@gmail.com>
2024-05-15 19:08:05 +0400

    Created Modal component to wrap NewPost component

--- react-memo/src/components/PostList.jsx
+++ react-memo/src/components/PostList.jsx
@@ -1,23 +1,36 @@
 import Post from "./Post";
 import NewPost from "./NewPost";
+import Modal from "./Modal";
 import classes from "./PostList.module.css";
 import { useState } from "react";
 
 function PostList(props) {
-  const [text, setText] = useState('');
-  const [author, setAuthor] = useState('');
+  const [text, setText] = useState("");
+  const [author, setAuthor] = useState("");
+  const [isModalVisible, setIsModalVisible] = useState(true);
 
-  function onTextChangeHandler(event){
+  function onTextChangeHandler(event) {
     setText(event.target.value);
   }
 
-  function onAuthorHandler(event){
+  function onAuthorHandler(event) {
     setAuthor(event.target.value);
+  }
+
+  function closeModalHandler() {
+    setIsModalVisible(false);
   }
 
   return (
     <>
-      <NewPost onTextChange={onTextChangeHandler} onAuthorChange={onAuthorHandler}/>
+      {isModalVisible && (
+        <Modal onClose={closeModalHandler}>
+          <NewPost
+            onTextChange={onTextChangeHandler}
+            onAuthorChange={onAuthorHandler}
+          />
+        </Modal>
+      )}
       <ul className={classes.posts}>
         {props.posts.map((post) => (
           <Post author={post.author} text={post.text} />
