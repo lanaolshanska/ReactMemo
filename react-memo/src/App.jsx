57b4b4deaf95c7b72d3346b67b8a175526524973
Svitlana Olshanska <svitlana.olshanska@gmail.com>
2024-05-16 17:33:23 +0400

    Implemented data fetching and adding new posts

--- react-memo/src/App.jsx
+++ react-memo/src/App.jsx
@@ -3,13 +3,6 @@ import Header from "./components/Header";
 import "./App.css";
 import { useState } from "react";
 
-const posts = [
-  { author: "TestAuthor", text: "text" },
-  { author: "TestAuthor2", text: "text2" },
-  { author: "TestAuthor3", text: "text3" },
-  { author: "TestAuthor4", text: "text4" },
-];
-
 function App() {
   const [isModalVisible, setIsModalVisible] = useState(false);
 
@@ -25,7 +18,7 @@ function App() {
     <>
       <Header onCreatePost={openModalHandler} />
       <main>
-        <PostList data={posts} isModalVisible={isModalVisible} onCloseModal={closeModalHandler} />
+        <PostList isModalVisible={isModalVisible} onCloseModal={closeModalHandler} />
       </main>
     </>
   );
--- react-memo/src/components/PostList.jsx
+++ react-memo/src/components/PostList.jsx
@@ -2,12 +2,31 @@ import Post from "./Post";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
 import classes from "./PostList.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-function PostList({ data, isModalVisible, onCloseModal }) {
-  const [posts, setPosts] = useState(data);
+function PostList({ isModalVisible, onCloseModal }) {
+  const url = "http://localhost:8080/";
+  const [isLoading, setIsLoading] = useState(true);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function getPosts() {
+      const response = await fetch(url + "posts");
+      const data = await response.json();
+      setPosts(data.posts);
+      setIsLoading(false);
+    }
+    getPosts();
+  }, []);
 
   function addNewPostHandler(newPost) {
+    fetch(url + "posts", {
+      method: "POST",
+      body: JSON.stringify(newPost),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     setPosts((existingPosts) => [...existingPosts, newPost]);
   }
 
@@ -18,11 +37,20 @@ function PostList({ data, isModalVisible, onCloseModal }) {
           <NewPost onClose={onCloseModal} onAddNewPost={addNewPostHandler} />
         </Modal>
       )}
-      <ul className={classes.posts}>
-        {posts.map((post, index) => (
-          <Post key={index} author={post.author} text={post.text} />
-        ))}
-      </ul>
+      {!isLoading && posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.id} author={post.author} text={post.text} />
+          ))}
+        </ul>
+      )}
+      {!isLoading && posts.length === 0 && (
+        <>
+          <h2>There are no posts yet</h2>
+          <p>Start adding some!</p>
+        </>
+      )}
+      {isLoading && <p>Loading posts...</p>}
     </>
   );
 }
